df02cd004b0e781938107a55796f2deb2dc8dc5f
syarifhasibuan <syarifhsb.dev@gmail.com>
2024-12-23 02:06:21 +0100

    feat: Using state to rerender task card. No need to refresh them

--- src/components/features/task-card.tsx
+++ src/components/features/task-card.tsx
@@ -35,6 +35,10 @@ export function TaskCardItem({ task }: { task: Task }) {
   const tasks = useTasks();
   const [date, setDate] = React.useState<Date>();
   const [datepickerOpen, setDatepickerOpen] = React.useState(false);
+  const [isTaskActive, setIsTaskActive] = React.useState(true);
+
+  //   Question: SetCategory updates the UI, but linter does not like this method of updating the state.
+  const [, setCategory] = React.useState("");
 
   function handleDateButtonClick(date: Date) {
     setDate(date);
@@ -42,151 +46,161 @@ export function TaskCardItem({ task }: { task: Task }) {
     task.date = date;
     saveTasks(tasks.map((t) => (t.id === task.id ? task : t)));
   }
-  function handleChangeCategory(category: string) {
+
+  function handleChangeCategory(cat: string) {
     return () => {
-      if (category === "remove") {
+      if (cat === "remove") {
+        setCategory("");
         task.category = "";
       } else {
-        task.category = category;
+        setCategory(cat);
+        task.category = cat;
       }
+
       saveTasks(tasks.map((t) => (t.id === task.id ? task : t)));
-      //   Question: How to update the task category on UI without reloading the page?
-      window.location.href = `/`;
     };
   }
 
   function handleDelete(task: Task) {
     return () => {
+      setIsTaskActive(false);
       saveTasks(tasks.filter((t) => t.id !== task.id));
-      window.location.href = `/`;
     };
   }
 
   return (
-    <li>
-      <Card className="min-w-[250px]">
-        <CardContent className="p-3">
-          <div className="flex flex-row gap-2 justify-between">
-            <div>
-              <CardTitle className="text-lg">{task.title}</CardTitle>
-              <CardDescription>{task.description}</CardDescription>
-            </div>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  className="h-6 w-6 data-[state=open]:bg-accent"
-                  variant="ghost"
-                  size="sm"
-                >
-                  <EllipsisIcon />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="min-w-32 bg-popover text-popover-foreground rounded-sm shadow-lg cursor-pointer p-1 border">
-                <DropdownMenuItem className="flex flex-row gap-2 p-0.5 items-center rounded-sm hover:bg-primary focus:bg-primary outline-none">
-                  <PencilIcon className="h-3 w-3" />
-                  <p className="text-sm">Edit</p>
-                  <DropdownMenuShortcut>m</DropdownMenuShortcut>
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  className="flex flex-row gap-2 p-0.5 items-center rounded-sm hover:bg-primary focus:bg-primary outline-none"
-                  onSelect={handleDelete(task)}
-                >
-                  <TrashIcon className="h-3 w-3" />
-                  <p className="text-sm">Delete</p>
-                  <DropdownMenuShortcut>d</DropdownMenuShortcut>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-          <div className="flex flex-row gap-2 mt-1">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  className="gap-1 h-6 data-[state=open]:bg-accent p-0.5 border"
-                  variant="ghost"
-                  size="sm"
-                >
-                  <GroupIcon />
-                  <p className="text-xs">{task.category || "No Category"}</p>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="min-w-20 bg-popover text-popover-foreground rounded-sm shadow-lg cursor-pointer p-1 border">
-                <DropdownMenuItem
-                  onSelect={handleChangeCategory("remove")}
-                  className="flex flex-row gap-1 p-0.5 items-center rounded-sm hover:bg-primary focus:bg-primary outline-none"
-                  key="remove"
-                >
-                  <TrashIcon className="h-3 w-3" />
-                  <p className="text-xs">Remove Category</p>
-                </DropdownMenuItem>
-                {categories.map((category) => (
-                  <DropdownMenuItem
-                    onSelect={handleChangeCategory(category)}
-                    className="flex flex-row gap-1 p-0.5 items-center rounded-sm hover:bg-primary focus:bg-primary outline-none"
-                    key={category}
-                  >
-                    <GroupIcon className="h-3 w-3" />
-                    <p className="text-xs">{category}</p>
-                  </DropdownMenuItem>
-                ))}
-              </DropdownMenuContent>
-            </DropdownMenu>
-            <Popover open={datepickerOpen} onOpenChange={setDatepickerOpen}>
-              <PopoverTrigger asChild>
-                <Button
-                  variant="outline"
-                  className="text-xs gap-1 h-6 data-[state=open]:bg-accent p-0.5 border"
-                >
-                  <CalendarIcon className="h-3 w-3" />
-                  <p>{task.date ? format(task.date, "PPP") : "No Deadline"}</p>
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent
-                className="bg-popover text-popover-foreground rounded-sm shadow-lg p-1 border"
-                align="center"
-              >
-                <div className="flex flex-row gap-1 ">
-                  <Button
-                    variant="secondary"
-                    onClick={() => handleDateButtonClick(new Date())}
-                  >
-                    Today
-                  </Button>
-                  <Button
-                    variant="secondary"
-                    onClick={() =>
-                      handleDateButtonClick(addDays(new Date(), 1))
-                    }
-                  >
-                    Tomorrow
-                  </Button>
-                  <Button
-                    variant="secondary"
-                    onClick={() =>
-                      handleDateButtonClick(addDays(new Date(), 7))
-                    }
-                  >
-                    Next Week
-                  </Button>
+    <>
+      {isTaskActive ? (
+        <li>
+          <Card className="min-w-[250px]">
+            <CardContent className="p-3">
+              <div className="flex flex-row gap-2 justify-between">
+                <div>
+                  <CardTitle className="text-lg">{task.title}</CardTitle>
+                  <CardDescription>{task.description}</CardDescription>
                 </div>
-                <Calendar
-                  className="rounded-md border shadow"
-                  mode="single"
-                  selected={date}
-                  onSelect={(newDate) => {
-                    setDate(newDate);
-                    setDatepickerOpen(false);
-                    if (newDate) {
-                      handleDateButtonClick(newDate);
-                    }
-                  }}
-                  initialFocus
-                />
-              </PopoverContent>
-            </Popover>
-          </div>
-        </CardContent>
-      </Card>
-    </li>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button
+                      className="h-6 w-6 data-[state=open]:bg-accent"
+                      variant="ghost"
+                      size="sm"
+                    >
+                      <EllipsisIcon />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="min-w-32 bg-popover text-popover-foreground rounded-sm shadow-lg cursor-pointer p-1 border">
+                    <DropdownMenuItem className="flex flex-row gap-2 p-0.5 items-center rounded-sm hover:bg-primary focus:bg-primary outline-none">
+                      <PencilIcon className="h-3 w-3" />
+                      <p className="text-sm">Edit</p>
+                      <DropdownMenuShortcut>m</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                      className="flex flex-row gap-2 p-0.5 items-center rounded-sm hover:bg-primary focus:bg-primary outline-none"
+                      onSelect={handleDelete(task)}
+                    >
+                      <TrashIcon className="h-3 w-3" />
+                      <p className="text-sm">Delete</p>
+                      <DropdownMenuShortcut>d</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+              <div className="flex flex-row gap-2 mt-1">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button
+                      className="gap-1 h-6 data-[state=open]:bg-accent p-0.5 border"
+                      variant="ghost"
+                      size="sm"
+                    >
+                      <GroupIcon />
+                      <p className="text-xs">
+                        {task.category || "No Category"}
+                      </p>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="min-w-20 bg-popover text-popover-foreground rounded-sm shadow-lg cursor-pointer p-1 border">
+                    <DropdownMenuItem
+                      onSelect={handleChangeCategory("remove")}
+                      className="flex flex-row gap-1 p-0.5 items-center rounded-sm hover:bg-primary focus:bg-primary outline-none"
+                      key="remove"
+                    >
+                      <TrashIcon className="h-3 w-3" />
+                      <p className="text-xs">Remove Category</p>
+                    </DropdownMenuItem>
+                    {categories.map((cat) => (
+                      <DropdownMenuItem
+                        onSelect={handleChangeCategory(cat)}
+                        className="flex flex-row gap-1 p-0.5 items-center rounded-sm hover:bg-primary focus:bg-primary outline-none"
+                        key={cat}
+                      >
+                        <GroupIcon className="h-3 w-3" />
+                        <p className="text-xs">{cat}</p>
+                      </DropdownMenuItem>
+                    ))}
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <Popover open={datepickerOpen} onOpenChange={setDatepickerOpen}>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant="outline"
+                      className="text-xs gap-1 h-6 data-[state=open]:bg-accent p-0.5 border"
+                    >
+                      <CalendarIcon className="h-3 w-3" />
+                      <p>
+                        {task.date ? format(task.date, "PPP") : "No Deadline"}
+                      </p>
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent
+                    className="bg-popover text-popover-foreground rounded-sm shadow-lg p-1 border"
+                    align="center"
+                  >
+                    <div className="flex flex-row gap-1 ">
+                      <Button
+                        variant="secondary"
+                        onClick={() => handleDateButtonClick(new Date())}
+                      >
+                        Today
+                      </Button>
+                      <Button
+                        variant="secondary"
+                        onClick={() =>
+                          handleDateButtonClick(addDays(new Date(), 1))
+                        }
+                      >
+                        Tomorrow
+                      </Button>
+                      <Button
+                        variant="secondary"
+                        onClick={() =>
+                          handleDateButtonClick(addDays(new Date(), 7))
+                        }
+                      >
+                        Next Week
+                      </Button>
+                    </div>
+                    <Calendar
+                      className="rounded-md border shadow"
+                      mode="single"
+                      selected={date}
+                      onSelect={(newDate) => {
+                        setDate(newDate);
+                        setDatepickerOpen(false);
+                        if (newDate) {
+                          handleDateButtonClick(newDate);
+                        }
+                      }}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+              </div>
+            </CardContent>
+          </Card>
+        </li>
+      ) : null}
+    </>
   );
 }
