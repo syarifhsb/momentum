424fe802adf93562b280100e241baa619e537e5a
syarifhasibuan <syarifhsb.dev@gmail.com>
2024-12-23 01:21:30 +0100

    feat: adjust for mobile

--- src/task-management.tsx
+++ src/task-management.tsx
@@ -7,7 +7,7 @@ export function TaskManagement() {
     <div className="p-3">
       <HeadingOne className="text-center p-2">Momentum</HeadingOne>
       <p className="text-center p-2 pt-0">A simple task management app.</p>
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-col items-center gap-4 md:flex-row md:items-start">
         <NewTaskForm />
         <ExistingTasks />
       </div>
