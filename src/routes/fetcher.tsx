b6a08901995bf7731be1fa32dbce914c159b0cb3
M Haidar Hanif <git@mhaidarhanif.com>
2024-12-29 17:07:45 +0700

    feat: refine fetcher

--- src/routes/fetcher.tsx
+++ src/routes/fetcher.tsx
@@ -3,28 +3,17 @@ import useSWR from "swr";
 import { HeadingOne } from "@/components/ui/typography";
 import { Debug } from "@/components/ui/debug";
 
-function FetchItem() {
-  const fetcher = (url: any) => fetch(url).then((r) => r.json());
-  const { data, error, isLoading } = useSWR(
-    "https://amazingsafari-backend.haidar.dev/",
-    fetcher,
-  );
-
-  if (error) return <div>Failed to fetch data</div>;
-
-  if (isLoading) return <div>Loading...</div>;
-
-  console.log(data);
-  return <Debug>{data}</Debug>;
-}
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export function FetcherRoute() {
   return (
     <div className="flex flex-col items-center space-y-4">
       <HeadingOne>Data Fetching</HeadingOne>
+
       <p className="text-center p-2 pt-0">
         A simple data fetching example to demonstrate the use of SWR hook.
       </p>
+
       <p className="text-center p-2 pt-0">
         Data fetched from{" "}
         <a
@@ -35,7 +24,21 @@ export function FetcherRoute() {
           https://amazingsafari-backend.haidar.dev
         </a>
       </p>
+
       <FetchItem />
     </div>
   );
+}
+
+function FetchItem() {
+  const { data, error, isLoading } = useSWR(
+    "https://amazingsafari-backend.haidar.dev/products",
+    fetcher,
+  );
+
+  if (error) return <div>Failed to fetch data</div>;
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return <Debug>{data}</Debug>;
 }
